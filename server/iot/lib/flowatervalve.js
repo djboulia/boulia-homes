5e8b2c900a6adc543791e33ca1dcb18e25d44b4b
Don Boulia <djboulia@gmail.com>
2022-05-16 20:53:31 -0400

    only init once per session vs. re-establshing a new session each refresh

--- server/iot/lib/flowatervalve.js
+++ server/iot/lib/flowatervalve.js
@@ -19,10 +19,24 @@ const FloWaterValve = function (email, password) {
 
     const flo = new Flo(logger, config, undefined, console.debug);
 
+    this.initComplete = false;
+
     this.init = function () {
+        const self = this;
+
         return new Promise((resolve, reject) => {
-            flo.init()
+            if (self.initComplete) {
+                console.log("flo initialized previously");
+                resolve(true);
+            } else {
+                console.log("flo initialization starting");
+
+                flo.init()
                 .then((result) => {
+                    console.log("flo initialization complete");
+
+                    self.initComplete = true;
+    
                     // only call discoverDevices() once... multiple
                     // calls will continue adding to the flo_devices array
                     return flo.discoverDevices();
@@ -34,6 +48,7 @@ const FloWaterValve = function (email, password) {
                 .catch((e) => {
                     reject(e);
                 })
+            }
         });
     }
 
@@ -45,12 +60,20 @@ const FloWaterValve = function (email, password) {
 
     }
 
+    /**
+     * find the device structure required for the refreshDevice call
+     * [4/30/2022] djb changed this method to return device index vs.
+     *                 deviceid
+     * 
+     * @param {string} id device id to find
+     * @returns the index of the device in the device list
+     */
     const findDevice = function (id) {
         for (let i = 0; i < flo.flo_devices.length; i++) {
             const device = flo.flo_devices[i];
 
             if (device.deviceid === id) {
-                return device;
+                return i;
             }
         }
 
@@ -59,17 +82,20 @@ const FloWaterValve = function (email, password) {
 
     this.isValveOpen = function (id) {
         return new Promise((resolve, reject) => {
-            const device = findDevice(id);
+            const index = findDevice(id);
+            const device = (index===undefined) ? undefined : flo.flo_devices[index];
             
-            if (device === undefined) {
+            if (index === undefined) {
                 reject('device id ' + id + ' not found!');
                 return;
             }
 
-            flo.refreshDevice(device)
+            console.log('flo device: ', device);
+
+            flo.refreshDevice(index)
                 .then((result) => {
 
-                    console.log("waterValve ", device);
+                    console.log("waterValve ", id);
                     resolve((device.valveCurrentState === 'closed') ? false : true);
                 })
                 .catch((err) => {
