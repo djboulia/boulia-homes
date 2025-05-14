9e37699cc542ef85251bbb074440c98e178a9dbb
Don Boulia <djboulia@gmail.com>
2025-05-14 09:24:00 -0400

    remove unused import

--- server/iot/lib/nest-thermostat.js
+++ server/iot/lib/nest-thermostat.js
@@ -1,5 +1,3 @@
-const axios = require('axios');
-
 const TRAIT_ECO = "sdm.devices.traits.ThermostatEco";
 const TRAIT_TEMP = "sdm.devices.traits.Temperature";
 const TRAIT_HVAC = "sdm.devices.traits.ThermostatHvac";
@@ -10,8 +8,10 @@ const KEY_TEMP_AMBIENT = "ambientTemperatureCelsius";
 
 const CMD_ECO = "sdm.devices.commands.ThermostatEco.SetMode";
 const CMD_MODE = "sdm.devices.commands.ThermostatMode.SetMode";
-const CMD_HEAT_TEMP = "sdm.devices.commands.ThermostatTemperatureSetpoint.SetHeat";
-const CMD_COOL_TEMP = "sdm.devices.commands.ThermostatTemperatureSetpoint.SetCool";
+const CMD_HEAT_TEMP =
+  "sdm.devices.commands.ThermostatTemperatureSetpoint.SetHeat";
+const CMD_COOL_TEMP =
+  "sdm.devices.commands.ThermostatTemperatureSetpoint.SetCool";
 
 const PARAM_MODE = "mode";
 const PARAM_HEAT = "heatCelsius";
@@ -21,199 +21,201 @@ const VAL_MANUAL_ECO = "MANUAL_ECO";
 const VAL_OFF = "OFF";
 
 const toFahrenheit = function (tempInC) {
-    return (tempInC * 1.8) + 32;
-}
+  return tempInC * 1.8 + 32;
+};
 
 const toCelsius = function (tempInF) {
-    return (tempInF - 32) * 5 / 9;
-}
+  return ((tempInF - 32) * 5) / 9;
+};
 
 const NestThermostat = function (nest, deviceId, deviceInfo) {
-    this.isEcoMode = function () {
-        // console.log('eco ', deviceInfo.traits[TRAIT_ECO]);
+  this.isEcoMode = function () {
+    // console.log('eco ', deviceInfo.traits[TRAIT_ECO]);
 
-        return deviceInfo.traits[TRAIT_ECO].mode === VAL_MANUAL_ECO;
+    return deviceInfo.traits[TRAIT_ECO].mode === VAL_MANUAL_ECO;
+  };
+
+  this.ecoHeat = function () {
+    // console.log('eco ', deviceInfo.traits[TRAIT_ECO]);
+
+    const eco = deviceInfo.traits[TRAIT_ECO];
+    return toFahrenheit(eco[PARAM_HEAT]);
+  };
+
+  this.ecoCool = function () {
+    // console.log('eco ', deviceInfo.traits[TRAIT_ECO]);
+
+    const eco = deviceInfo.traits[TRAIT_ECO];
+    return toFahrenheit(eco[PARAM_COOL]);
+  };
+
+  this.getTemperature = function () {
+    // console.log('device info ', deviceInfo);
+
+    const temp = deviceInfo.traits[TRAIT_TEMP];
+    return toFahrenheit(temp[KEY_TEMP_AMBIENT]);
+  };
+
+  this.getTemperatureSetPoint = function () {
+    // console.log('device info ', deviceInfo);
+
+    const temp = deviceInfo.traits[TRAIT_SET_POINT];
+
+    if (temp[PARAM_HEAT]) {
+      return toFahrenheit(temp[PARAM_HEAT]);
+    } else if (temp[PARAM_COOL]) {
+      return toFahrenheit(temp[PARAM_COOL]);
     }
 
-    this.ecoHeat = function () {
-        // console.log('eco ', deviceInfo.traits[TRAIT_ECO]);
+    return undefined;
+  };
 
-        const eco = deviceInfo.traits[TRAIT_ECO];
-        return toFahrenheit(eco[PARAM_HEAT]);
-    }
+  this.getHvacStatus = function () {
+    // console.log('device info ', deviceInfo);
 
-    this.ecoCool = function () {
-        // console.log('eco ', deviceInfo.traits[TRAIT_ECO]);
+    const val = deviceInfo.traits[TRAIT_HVAC].status;
+    return val;
+  };
 
-        const eco = deviceInfo.traits[TRAIT_ECO];
-        return toFahrenheit(eco[PARAM_COOL]);
-    }
+  this.getMode = function () {
+    // console.log('device info ', deviceInfo);
 
-    this.getTemperature = function () {
-        // console.log('device info ', deviceInfo);
+    const val = deviceInfo.traits[TRAIT_MODE];
+    return val.mode;
+  };
 
-        const temp = deviceInfo.traits[TRAIT_TEMP];
-        return toFahrenheit(temp[KEY_TEMP_AMBIENT]);
-    }
+  this.getAvailableModes = function () {
+    // console.log('device info ', deviceInfo);
 
-    this.getTemperatureSetPoint = function () {
-        // console.log('device info ', deviceInfo);
+    const val = deviceInfo.traits[TRAIT_MODE];
+    return val.availableModes;
+  };
 
-        const temp = deviceInfo.traits[TRAIT_SET_POINT];
+  /**
+   * set the thermostat mode to HEAT, COOL, or OFF
+   */
+  this.setMode = function (strMode) {
+    const self = this;
 
-        if (temp[PARAM_HEAT]) {
-            return toFahrenheit(temp[PARAM_HEAT]);
-        } else if (temp[PARAM_COOL]) {
-            return toFahrenheit(temp[PARAM_COOL]);
-        }
+    return new Promise((resolve, reject) => {
+      if (strMode != "HEAT" && strMode != "COOL" && strMode != "OFF") {
+        const err = "Invalid mode setting " + strMode;
+        console.log(err);
+        reject(err);
+        return;
+      }
 
-        return undefined;
-    }
+      const params = {};
+      params[PARAM_MODE] = strMode;
 
-    this.getHvacStatus = function () {
-        // console.log('device info ', deviceInfo);
+      nest
+        .command(deviceId, CMD_MODE, params)
+        .then((res) => {
+          const result = res.data;
 
-        const val = deviceInfo.traits[TRAIT_HVAC].status;
-        return val;
-    }
+          const val = deviceInfo.traits[TRAIT_MODE];
+          val.mode = strMode;
 
-    this.getMode = function () {
-        // console.log('device info ', deviceInfo);
-
-        const val = deviceInfo.traits[TRAIT_MODE];
-        return val.mode;
-    }
-
-    this.getAvailableModes = function () {
-        // console.log('device info ', deviceInfo);
-
-        const val = deviceInfo.traits[TRAIT_MODE];
-        return val.availableModes;
-    }
-
-    /**
-     * set the thermostat mode to HEAT, COOL, or OFF
-     */
-    this.setMode = function (strMode) {
-        const self = this;
-
-        return new Promise((resolve, reject) => {
-
-            if (strMode != 'HEAT' && strMode != 'COOL' && strMode != 'OFF') {
-                const err = "Invalid mode setting " + strMode;
-                console.log(err);
-                reject(err);
-                return;
-            }
-
-            const params = {};
-            params[PARAM_MODE] = strMode;
-
-            nest.command(deviceId, CMD_MODE, params)
-                .then((res) => {
-                    const result = res.data;
-
-                    const val = deviceInfo.traits[TRAIT_MODE];
-                    val.mode = strMode;
-            
-                    resolve(self);
-                })
-                .catch((e) => {
-                    reject(e);
-                })
+          resolve(self);
         })
-    }
+        .catch((e) => {
+          reject(e);
+        });
+    });
+  };
 
-    this.setHeatTemperature = function (temp) {
-        const self = this;
+  this.setHeatTemperature = function (temp) {
+    const self = this;
 
-        const tempCelsius = toCelsius(temp);
-        console.log('temp ' + temp + ', celsius=', tempCelsius);
+    const tempCelsius = toCelsius(temp);
+    console.log("temp " + temp + ", celsius=", tempCelsius);
 
-        return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
+      if (self.getMode() != "HEAT") {
+        const err =
+          "setHeatTemperature: thermostat not in HEAT mode" + self.getMode();
+        console.err(err);
+        reject(err);
+        return;
+      }
 
-            if (self.getMode() != 'HEAT') {
-                const err = "setHeatTemperature: thermostat not in HEAT mode" + self.getMode();
-                console.err(err);
-                reject(err);
-                return;
-            }
+      const params = {};
+      params[PARAM_HEAT] = tempCelsius;
 
-            const params = {};
-            params[PARAM_HEAT] = tempCelsius;
+      nest
+        .command(deviceId, CMD_HEAT_TEMP, params)
+        .then((res) => {
+          const result = res.data;
 
-            nest.command(deviceId, CMD_HEAT_TEMP, params)
-                .then((res) => {
-                    const result = res.data;
+          console.log("result: ", result);
+          deviceInfo.traits[TRAIT_SET_POINT] = { PARAM_HEAT: temp };
 
-                    console.log('result: ', result);
-                    deviceInfo.traits[TRAIT_SET_POINT] = { PARAM_HEAT: temp };
-
-                    resolve(self);
-                })
-                .catch((e) => {
-                    reject(e);
-                })
+          resolve(self);
         })
-    }
+        .catch((e) => {
+          reject(e);
+        });
+    });
+  };
 
-    this.setCoolTemperature = function (temp) {
-        const self = this;
+  this.setCoolTemperature = function (temp) {
+    const self = this;
 
-        const tempCelsius = toCelsius(temp);
-        console.log('temp ' + temp + ', celsius=', tempCelsius);
+    const tempCelsius = toCelsius(temp);
+    console.log("temp " + temp + ", celsius=", tempCelsius);
 
-        return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
+      if (self.getMode() != "COOL") {
+        const err =
+          "setCoolTemperature: thermostat not in COOL mode" + self.getMode();
+        console.err(err);
+        reject(err);
+        return;
+      }
 
-            if (self.getMode() != 'COOL') {
-                const err = "setCoolTemperature: thermostat not in COOL mode" + self.getMode();
-                console.err(err);
-                reject(err);
-                return;
-            }
+      const params = {};
+      params[PARAM_COOL] = tempCelsius;
 
-            const params = {};
-            params[PARAM_COOL] = tempCelsius;
+      nest
+        .command(deviceId, CMD_COOL_TEMP, params)
+        .then((res) => {
+          const result = res.data;
 
-            nest.command(deviceId, CMD_COOL_TEMP, params)
-                .then((res) => {
-                    const result = res.data;
+          console.log("result: ", result);
+          deviceInfo.traits[TRAIT_SET_POINT] = { PARAM_COOL: temp };
 
-                    console.log('result: ', result);
-                    deviceInfo.traits[TRAIT_SET_POINT] = { PARAM_COOL: temp };
-
-                    resolve(self);
-                })
-                .catch((e) => {
-                    reject(e);
-                })
+          resolve(self);
         })
-    }
+        .catch((e) => {
+          reject(e);
+        });
+    });
+  };
 
-    this.setEcoMode = function (on) {
-        const self = this;
+  this.setEcoMode = function (on) {
+    const self = this;
 
-        return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
+      const params = {};
+      params[PARAM_MODE] = on ? VAL_MANUAL_ECO : VAL_OFF;
 
-            const params = {};
-            params[PARAM_MODE] = (on) ? VAL_MANUAL_ECO : VAL_OFF;
+      nest
+        .command(deviceId, CMD_ECO, params)
+        .then((res) => {
+          const result = res.data;
 
-            nest.command(deviceId, CMD_ECO, params)
-                .then((res) => {
-                    const result = res.data;
+          console.log("eco result: ", result);
 
-                    console.log('eco result: ', result);
-
-                    // update our internal status
-                    deviceInfo.traits[TRAIT_ECO].mode = (on) ? VAL_MANUAL_ECO : VAL_OFF;
-                    resolve(self);
-                })
-                .catch((e) => {
-                    console.log('nest command error: ', e.response);
-                    reject(e);
-                })
+          // update our internal status
+          deviceInfo.traits[TRAIT_ECO].mode = on ? VAL_MANUAL_ECO : VAL_OFF;
+          resolve(self);
         })
-    }
-}
+        .catch((e) => {
+          console.log("nest command error: ", e.response);
+          reject(e);
+        });
+    });
+  };
+};
 
 module.exports = NestThermostat;
